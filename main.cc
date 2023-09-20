2f5057ad8e50c1c3ce6d15181adc39ed7cd198cc
Albaro Pereyra <141711575+albaropereyra22@users.noreply.github.com>
2023-09-20 00:16:44 -0700

    working sample of upload file on Windows.

--- main.cc
+++ main.cc
@@ -1,10 +1,46 @@
 #include <drogon/drogon.h>
-int main() {
-    //Set HTTP listener address and port
-    drogon::app().addListener("0.0.0.0",80);
-    //Load config file
-    //drogon::app().loadConfigFile("../config.json");
-    //Run HTTP framework,the method will block in the internal event loop
-    drogon::app().run();
-    return 0;
+using namespace drogon;
+
+int main()
+{
+    app().registerHandler(
+        "/",
+        [](const HttpRequestPtr &,
+           std::function<void(const HttpResponsePtr &)> &&callback) {
+            auto resp = HttpResponse::newHttpViewResponse("FileUpload.csp");
+            callback(resp);
+        });
+
+    app().registerHandler(
+        "/upload_endpoint",
+        [](const HttpRequestPtr &req,
+           std::function<void(const HttpResponsePtr &)> &&callback) {
+            MultiPartParser fileUpload;
+            if (fileUpload.parse(req) != 0 || fileUpload.getFiles().size() != 1)
+            {
+                auto resp = HttpResponse::newHttpResponse();
+                resp->setBody("Must only be one file");
+                resp->setStatusCode(k403Forbidden);
+                callback(resp);
+                return;
+            }
+
+            auto &file = fileUpload.getFiles()[0];
+            auto md5 = file.getMd5();
+            auto resp = HttpResponse::newHttpResponse();
+            resp->setBody(
+                "The server has calculated the file's MD5 hash to be " + md5);
+            file.save();
+            LOG_INFO << "The uploaded file has been saved to the ./uploads "
+                        "directory";
+            callback(resp);
+        },
+        {Post});
+
+    LOG_INFO << "Server running on 127.0.0.1:8848";
+    app()
+        .setClientMaxBodySize(20 * 2000 * 2000)
+        .setUploadPath("C:\\Users\\dell\\file_upload\\uploads")
+        .addListener("127.0.0.1", 8848)
+        .run();
 }
